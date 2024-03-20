1a2da0ef7b8ea4ed0b54c9e897e79f6031a52e1a
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-20 22:10:06 +0530

    updated

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,3 +1,25 @@
+let list = document.querySelectorAll('.carousel .list .item');
+let carousel = document.querySelector('.carousel');
+let dots = document.querySelectorAll('.dots li');
+let nextBtn = document.getElementById('next');
+let prevBtn = document.getElementById('prev');
+
+let lastPosition = list.length - 1;
+let active = 0;
+let zIndex = 2;
+
+
+
+
+
+
+
+
+
+
+
+
+
 (function ($) {
     "use strict";
 
