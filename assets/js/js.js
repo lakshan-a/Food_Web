021dbdcd019b16f2e6ae7a9cba3a4ece25cff841
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-21 19:54:51 +0530

    updated

--- assets/js/js.js
+++ assets/js/js.js
@@ -83,3 +83,18 @@ const showSlider = (type) => {
     });
 
 }(jQuery));
+
+(function ($) {
+    "use strict";
+
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 1000
+    });
+
+})(jQuery);
