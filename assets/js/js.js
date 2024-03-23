82324274815bd24243bc29e8466cfeb4c4c42d1a
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-23 12:39:55 +0530

    updated

--- assets/js/js.js
+++ assets/js/js.js
@@ -163,4 +163,25 @@ const showSlider = (type) => {
         time: 1000
     });
 
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: true,
+        margin: 24,
+        dots: true,
+        loop: true,
+        nav : false,
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+
 })(jQuery);
